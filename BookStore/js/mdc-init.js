19d0c64f6e25ccac74cf2091530066dd018127a7
SaiHemanthBR <ssgahemanth15@hotmail.com>
2019-07-11 11:46:50 +0530

    Added Login

--- BookStore/js/mdc-init.js
+++ BookStore/js/mdc-init.js
@@ -5,3 +5,23 @@ for(var i = 0; i < MDCButtons.length; i++)
 var MDCTextFields = document.querySelectorAll('.mdc-text-field');
 for(var i = 0; i < MDCTextFields.length; i++)
   mdc.textField.MDCTextField.attachTo(MDCTextFields[i]);
+
+function isMacintosh() {
+  return navigator.platform.indexOf('Mac') > -1
+}
+
+function isWindows() {
+  return navigator.platform.indexOf('Win') > -1
+}
+
+if(isMacintosh()) {
+  var hidden = document.querySelector('.hide-on-mac')
+  for(var i = 0; i < hidden.length; i++) {
+    hidden[i].style.visibility = "hidden";
+  }
+} else {
+  var hidden = document.querySelectorAll('.show-on-mac')
+  for(var i = 0; i < hidden.length; i++) {
+    hidden[i].style.display = "none";
+  }
+}
