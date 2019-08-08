fe36451ac206364927a76ce2dcedb4843d8a72b3
SaiHemanthBR <sh.bheemreddy@icloud.com>
2019-08-08 22:20:21 +0530

    Added SF-Symbols

--- BookStore/js/coverflow.js
+++ BookStore/js/coverflow.js
@@ -5,9 +5,9 @@ var swiper = new Swiper('.swiper-container', {
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflow: {
-      rotate: 50,
+      rotate: 25,
       stretch: 0,
-      depth: 100,
+      depth: 200,
       modifier: 1,
       slideShadows : true
     },
